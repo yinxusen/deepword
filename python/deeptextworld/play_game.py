f4fc45bf455c69d6819052176792ce4a0623f399
Xusen Yin <yinxusen@gmail.com>
2019-03-31 17:27:45 -0700

    better display for play

--- python/deeptextworld/play_game.py
+++ python/deeptextworld/play_game.py
@@ -8,9 +8,9 @@ from textworld import EnvInfos
 def main(game_file):
     request_infos = EnvInfos()
     request_infos.description = True
-    request_infos.inventory = False
-    request_infos.entities = False
-    request_infos.verbs = False
+    request_infos.inventory = True
+    request_infos.entities = True
+    request_infos.verbs = True
     request_infos.max_score = True
     request_infos.has_won = True
     request_infos.extras = ["recipe"]
@@ -28,10 +28,18 @@ def main(game_file):
     dones = [False] * len(obs)
     print("max score is {}".format(infos["max_score"][0]))
     while not all(dones):
+        print("----------------------")
         print(obs[0])
+        print("----------------------")
         print("\n".join(infos["admissible_commands"][0]))
+        print("----------------------")
         print(infos["extra.recipe"][0])
+        print("----------------------")
         print("has won: {}".format(infos["has_won"][0]))
+        print("----------------------")
+        print(infos["verbs"][0])
+        print("----------------------")
+        print(infos["entities"][0])
         command = input("> ")
         obs, scores, dones, infos = env.step([command])
 
