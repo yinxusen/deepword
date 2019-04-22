060c30e64e39714c4f183ea48919dd646b1553f5
Xusen Yin <yinxusen@gmail.com>
2019-04-22 16:17:36 -0700

    update test bert drrn

--- bin/test-bert-drrn.sh
+++ bin/test-bert-drrn.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH --time=60:00:00
 #SBATCH --partition=isi
 
 set -e -x
 
-
 if [[ `hostname` =~ "hpc" ]]; then
-    PDIR=""
+    PDIR="$SLURM_SUBMIT_DIR"
+    filename="$SLURM_JOB_NAME"
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    export PYENV_ROOT="$HOME/local/lib/pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    if command -v pyenv 1>/dev/null 2>&1; then
+      eval "$(pyenv init -)"
+    fi
+    eval "$(pyenv virtualenv-init -)"
+    pyenv activate deepdnd-drrn
 else
     FWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
     PDIR="$FWDIR/.."
+    filename=$(basename "$0")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
 fi
 
 MODELHOME="$PDIR/../experiments-drrn/agent-drrn-test"
-BERT_CKPT_DIR="$MODELHOME/bert-model"
-
-VOCAB_FILE="$PDIR/resources/bert-vocab.txt"
+BERT_CKPT_DIR="$HOME/local/opt/bert-models/bert-model"
+VOCAB_FILE="$BERT_CKPT_DIR/vocab.txt"
+GAME_DIR="$PDIR/../textworld-competition-games/train-1"
 
 if [[ -f $HOME/local/etc/init_tensorflow.sh ]]; then
     source $HOME/local/etc/init_tensorflow.sh
@@ -30,7 +42,7 @@ fi
 pushd $PDIR
 ./bin/run.sh python/deeptextworld/main.py \
     -m $MODELHOME --mode train-drrn \
-    --game-path /Users/xusenyin/git-store/textworld-competition-games/train-1 \
+    --game-path $GAME_DIR \
     --vocab-file $VOCAB_FILE \
     --annealing-eps-t 300 --annealing-gamma-t 10 --observation-t 50 --replay-mem 100 \
     --eval-episode 1 \
