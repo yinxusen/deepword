cf293ff60439fecf3b632282d1937a11adc0aff8
Xusen Yin <yinxusen@gmail.com>
2019-10-10 19:02:24 -0700

    fix device placement

--- python/deeptextworld/dsqn_model.py
+++ python/deeptextworld/dsqn_model.py
@@ -393,14 +393,14 @@ def create_train_model(model_creator, hp, device_placement):
     with graph.as_default():
         with tf.device(device_placement):
             model = model_creator(hp)
-        initializer = tf.global_variables_initializer
-        inputs = model.inputs
-        q_actions = model.get_q_actions()
-        pred, diff_two_states = model.get_pred()
-        loss, train_op, abs_loss = model.get_train_op(q_actions)
-        snn_loss, snn_train_op = model.get_snn_train_op(pred)
-        weighted_loss, merged_train_op, s1, s2 = model.get_merged_train_op(
-            loss, snn_loss)
+            initializer = tf.global_variables_initializer
+            inputs = model.inputs
+            q_actions = model.get_q_actions()
+            pred, diff_two_states = model.get_pred()
+            loss, train_op, abs_loss = model.get_train_op(q_actions)
+            snn_loss, snn_train_op = model.get_snn_train_op(pred)
+            weighted_loss, merged_train_op, s1, s2 = model.get_merged_train_op(
+                loss, snn_loss)
         loss_summary = tf.summary.scalar("loss", loss)
         snn_loss_summary = tf.summary.scalar("snn_loss", snn_loss)
         weighted_loss_summary = tf.summary.scalar(
@@ -444,13 +444,13 @@ def create_eval_model(model_creator, hp, device_placement):
     with graph.as_default():
         with tf.device(device_placement):
             model = model_creator(hp, is_infer=True)
-        initializer = tf.global_variables_initializer
-        inputs = model.inputs
-        q_actions = model.get_q_actions()
-        pred, diff_two_states = model.get_pred()
-        loss, train_op, abs_loss = model.get_train_op(q_actions)
-        snn_loss, snn_train_op = model.get_snn_train_op(pred)
-        _ = model.get_merged_train_op(loss, snn_loss)
+            initializer = tf.global_variables_initializer
+            inputs = model.inputs
+            q_actions = model.get_q_actions()
+            pred, diff_two_states = model.get_pred()
+            loss, train_op, abs_loss = model.get_train_op(q_actions)
+            snn_loss, snn_train_op = model.get_snn_train_op(pred)
+            _ = model.get_merged_train_op(loss, snn_loss)
     return EvalDSQNModel(
         graph=graph, model=model, q_actions=q_actions, pred=pred,
         src_=inputs["src"],
