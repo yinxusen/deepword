8fd13cfc3dff2a1f668afad11d553ce62c582ee6
Xusen Yin <yinxusen@gmail.com>
2019-11-19 11:21:30 -0800

    add tjs cleaner

---
+++ python/deeptextworld/tools/clean_hs2tj.py
@@ -0,0 +1,79 @@
+import os
+from os.path import join as pjoin
+from threading import Thread
+
+import fire
+import numpy as np
+
+from deeptextworld.utils import eprint
+
+
+def clean_hs2tj(hash_states2tjs, tjs):
+    cnt_trashed = 0
+    empty_keys = []
+    all_tids = set(tjs.keys())
+    for k in hash_states2tjs.keys():
+        empty_tids = []
+        for tid in hash_states2tjs[k].keys():
+            if tid not in all_tids:
+                empty_tids.append(tid)
+                cnt_trashed += len(hash_states2tjs[k][tid])
+        for tid in empty_tids:
+            hash_states2tjs[k].pop(tid, None)
+        if hash_states2tjs[k] == {}:  # delete the dict if empty
+            empty_keys.append(k)
+    eprint("hs2tj deletes {} items".format(cnt_trashed))
+    for k in empty_keys:
+        hash_states2tjs.pop(k, None)
+    eprint("hs2tj deletes {} keys".format(len(empty_keys)))
+    return hash_states2tjs
+
+
+def load_snapshot(raw_tjs_path, hs2tj_path):
+    trajectories = {}
+    tjs = np.load(raw_tjs_path)
+    curr_tid = tjs["curr_tid"][0]
+    curr_tj = list(tjs["curr_tj"][0])
+    tids = tjs["tids"]
+    vals = tjs["vals"]
+    assert len(tids) == len(vals), "incompatible trajectory ids and values"
+    for i in range(len(tids)):
+        trajectories[tids[i]] = list(vals[i])
+    if curr_tid not in trajectories:
+        trajectories[curr_tid] = curr_tj
+
+    hs2tj = np.load(hs2tj_path)
+    hash_states2tjs = hs2tj["hs2tj"][0]
+
+    return trajectories, hash_states2tjs
+
+
+def clean_data(tp, hs):
+    eprint("cleaning\n{}\n{}".format(tp, hs))
+    hs_prefix = os.path.splitext(hs)[0]
+    tjs, hs2tj = load_snapshot(tp, hs)
+    cleaned_hs2tj = clean_hs2tj(hs2tj, tjs)
+    np.savez("{}.clean.npz".format(hs_prefix), hs2tj=[cleaned_hs2tj])
+
+
+def main(data_path, n_data):
+    tjs_prefix = "raw-trajectories"
+    hs2tj_prefix = "hs2tj"
+
+    combined_data_path = []
+    for i in range(n_data):
+        combined_data_path.append(
+            (pjoin(data_path, "{}-{}.npz".format(tjs_prefix, i)),
+             pjoin(data_path, "{}-{}.npz".format(hs2tj_prefix, i))))
+
+    threads = [
+        Thread(target=clean_data, args=(tp, hs))
+        for tp, hs in combined_data_path]
+
+    [t.start() for t in threads]
+
+    [t.join() for t in threads]
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
