f40d9cd74862d8c3dc3150c89e60a029759a1e12
Xusen Yin <yinxusen@gmail.com>
2020-09-14 22:50:23 -0700

    add docstrings for sum_tree

--- python/deepword/sum_tree.py
+++ python/deepword/sum_tree.py
@@ -1,120 +1,95 @@
+"""
+This SumTree code is modified version of Morvan Zhou:
+https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/blob/master/contents/5.2_Prioritized_Replay_DQN/RL_brain.py
+"""
+
+from typing import Tuple
+from typing import TypeVar, Generic
+
 import numpy as np
 
+E = TypeVar('E')  # type of the experience
 
-class SumTree(object):
-    """
-    This SumTree code is modified version of Morvan Zhou:
-    https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/blob/master/contents/5.2_Prioritized_Replay_DQN/RL_brain.py
-    """
-    data_pointer = 0
 
+class SumTree(Generic[E]):
     """
-    Here we initialize the tree with all nodes = 0, and initialize the data with all values = 0
+    The SumTree is a binary tree, with leaf nodes containing the real data.
     """
-
-    def __init__(self, capacity):
-        self.capacity = capacity  # Number of leaf nodes (final nodes) that contains experiences
-
+    def __init__(self, capacity: int):
+        # Number of leaf nodes (final nodes) that contains experiences
+        self.capacity = capacity
+        self.data_pointer = 0
         # Generate the tree with all nodes values = 0
-        # To understand this calculation (2 * capacity - 1) look at the schema above
-        # Remember we are in a binary node (each node has max 2 children) so 2x size of leaf (capacity) - 1 (root node)
+        # To understand this calculation (2 * capacity - 1)
+        # Remember we are in a binary node (each node has max 2 children)
+        # so 2x size of leaf (capacity) - 1 (root node)
         # Parent nodes = capacity - 1
         # Leaf nodes = capacity
         self.tree = np.zeros(2 * capacity - 1)
 
-        """ tree:
-            0
-           / \
-          0   0
-         / \ / \
-        0  0 0  0  [Size: capacity] it's at this line that there is the priorities score (aka pi)
-        """
-
         # Contains the experiences (so the size of data is capacity)
         self.data = np.zeros(capacity, dtype=object)
 
-    """
-    Here we add our priority score in the sumtree leaf and add the experience in data
-    """
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.tree[-self.capacity:])
 
-    def add(self, priority, data):
-        # Look at what index we want to put the experience
-        tree_index = self.data_pointer + self.capacity - 1
+    def add(self, priority: float, data: E):
+        """
+        Add an experience into the tree with a priority
 
-        """ tree:
-            0
-           / \
-          0   0
-         / \ / \
-tree_index  0 0  0  We fill the leaves from left to right
+        Args:
+            priority: priority of sampling
+            data: experience of the replay
+
+        Returns:
+            old data at the same position, 0 if unset
         """
         prev_data = self.data[self.data_pointer]
-        # Update data frame
         self.data[self.data_pointer] = data
 
+        # turn data_pointer to tree_index
+        tree_index = self.data_pointer + self.capacity - 1
         # Update the leaf
         self.update(tree_index, priority)
 
-        # Add 1 to data_pointer
+        # data_pointer moves to the next position
         self.data_pointer += 1
-
-        if self.data_pointer >= self.capacity:  # If we're above the capacity, you go back to first index (we overwrite)
+        # If we're above the capacity, go back to first index (we overwrite)
+        if self.data_pointer >= self.capacity:
             self.data_pointer = 0
         return prev_data
 
-    """
-    Update the leaf priority score and propagate the change through tree
-    """
+    def update(self, tree_index: int, priority: float) -> None:
+        """
+        Update the leaf priority score and propagate the change through tree
 
-    def update(self, tree_index, priority):
+        Args:
+            tree_index: tree index of the current data_pointer
+            priority: priority sampling value
+        """
         # Change = new priority score - former priority score
         change = priority - self.tree[tree_index]
         self.tree[tree_index] = priority
 
-        # then propagate the change through tree
-        while tree_index != 0:  # this method is faster than the recursive loop in the reference code
-
-            """
-            Here we want to access the line above
-            THE NUMBERS IN THIS TREE ARE THE INDEXES NOT THE PRIORITY VALUES
-
-                0
-               / \
-              1   2
-             / \ / \
-            3  4 5  [6] 
-
-            If we are in leaf at index 6, we updated the priority score
-            We need then to update index 2 node
-            So tree_index = (tree_index - 1) // 2
-            tree_index = (6-1)//2
-            tree_index = 2 (because // round the result)
-            """
+        # then propagate the change through tree, until the root
+        while tree_index != 0:
             tree_index = (tree_index - 1) // 2
             self.tree[tree_index] += change
 
-    """
-    Here we get the leaf_index, priority value of that leaf and experience associated with that index
-    """
-
-    def get_leaf(self, v):
+    def get_leaf(self, v: float) -> Tuple[int, float, E]:
         """
-        Tree structure and array storage:
-        Tree index:
-             0         -> storing priority sum
-            / \
-          1     2
-         / \   / \
-        3   4 5   6    -> storing priority for experiences
-        Array type for storing:
-        [0,1,2,3,4,5,6]
+        Get a leaf_index w.r.t. a priority value
+        the selected leaf_index must have the smallest priority among all leaves
+        that have larger priority values than v.
+
+        Args:
+            v: a priority value
+
+        Returns:
+            leaf index, priority, and experience associated with the leaf index
         """
         parent_index = 0
-
-        while True:  # the while loop is faster than the method in the reference code
+        while True:
             left_child_index = 2 * parent_index + 1
             right_child_index = left_child_index + 1
 
@@ -122,20 +97,19 @@ tree_index  0 0  0  We fill the leaves from left to right
             if left_child_index >= len(self.tree):
                 leaf_index = parent_index
                 break
-
-            else:  # downward search, always search for a higher priority node
-
+            else:  # downward search
                 if v <= self.tree[left_child_index]:
                     parent_index = left_child_index
-
                 else:
                     v -= self.tree[left_child_index]
                     parent_index = right_child_index
 
         data_index = leaf_index - self.capacity + 1
-
         return leaf_index, self.tree[leaf_index], self.data[data_index]
 
     @property
-    def total_priority(self):
-        return self.tree[0]  # Returns the root node
+    def total_priority(self) -> float:
+        """
+        The total priority is the value on the root node.
+        """
+        return self.tree[0]
